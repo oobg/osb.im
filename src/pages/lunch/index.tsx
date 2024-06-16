25b6f5b2aecaad6ad3b73b3a54f74f7f8dc7cc69
PKFlow <yoonseok.bae98@dotincorp.com>
2024-06-16 17:51:54 +0900

    feat: add /lunch page

---
+++ src/pages/lunch/index.tsx
@@ -0,0 +1,9 @@
+import { Lunch } from "@/features/lunch";
+
+export default function lunch() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center gap-5">
+			<Lunch />
+		</main>
+	);
+}
