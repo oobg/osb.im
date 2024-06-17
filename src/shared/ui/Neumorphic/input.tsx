4b15ab8979db2ac3a8e2f25e80a180aea73a55e8
PKFlow <yoonseok.bae98@dotincorp.com>
2024-06-17 12:40:32 +0900

    feat: add readOnly prop to Input component

--- src/shared/ui/Neumorphic/input.tsx
+++ src/shared/ui/Neumorphic/input.tsx
@@ -7,10 +7,11 @@ interface NewmorphicInputProps {
 	value: string,
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
 	onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+	readOnly?: boolean,
 }
 
 export default function Input({
-	type = "text", placeholder = "", className = "", value, onChange, onKeyPress,
+	type = "text", placeholder = "", className = "", value, onChange, onKeyPress, readOnly = false,
 }: Readonly<NewmorphicInputProps>) {
 	const combinedClassName = `neumorphic ${className}`.trim();
 	return (
@@ -21,6 +22,7 @@ export default function Input({
 			value={value}
 			onChange={onChange}
 			onKeyPress={onKeyPress}
+			readOnly={readOnly}
 		/>
 	);
 }
