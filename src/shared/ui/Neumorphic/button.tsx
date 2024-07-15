ec5aeeeb10864534b6fb57ac97af66cbf48ad3a6
PKFlow <yoonseok.bae98@dotincorp.com>
2024-07-15 21:24:10 +0900

    feat: add type prop to Neumorphic Button component
    - Add type prop to NewmorphicButtonProps interface with options "button", "submit", and "reset"
    - Update Button component to accept and apply the type prop, defaulting to "button"
    - Ensure the button type can be dynamically set when using the component

--- src/shared/ui/Neumorphic/button.tsx
+++ src/shared/ui/Neumorphic/button.tsx
@@ -4,15 +4,16 @@ interface NewmorphicButtonProps {
 	children: React.ReactNode,
 	className?: string,
 	onClick?: () => void;
+	type?: "button" | "submit" | "reset";
 }
 
 export default function Button({
-	children, className = "", onClick,
+	children, className = "", onClick, type = "button"
 }: Readonly<NewmorphicButtonProps>) {
 	const combinedClassName = `neumorphic ${className}`.trim();
 	return (
 		<>
-			<button className={combinedClassName} onClick={onClick}>
+			<button className={combinedClassName} onClick={onClick} type={type}>
 				<span className="text">{children}</span>
 			</button>
 		</>
